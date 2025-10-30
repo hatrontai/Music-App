1b125a139edcbd4c07d5f6f841f98776586e1961
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-10-31 00:18:41 +0700

    design list musics

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ sidebarPlus.onclick = function() {
     const sidebarPlusBtn = $('.sidebar__plus--btn');
     sidebarPlusBtn.classList.toggle('active');
 }
+
+const sections = $$('.main-content .section__content')
+sections.forEach((section) => {
+    section.addEventListener('mouseover', function() {
+        const rightBtn = section.querySelector('.section__right')
+        const leftBtn = section.querySelector('.section__left')
+        rightBtn.classList.add('active');
+        leftBtn.classList.add('active');
+    })
+    section.addEventListener('mouseout', function() {
+        const rightBtn = section.querySelector('.section__right')
+        const leftBtn = section.querySelector('.section__left')
+        rightBtn.classList.remove('active');
+        leftBtn.classList.remove('active');
+    })
+})
+
+const sectionItems = $$('.main-content .section__content--item');
+sectionItems.forEach((item) => {
+    item.addEventListener('mouseover', function() {
+        const playBtn = item.querySelector('.section__item--play');
+        playBtn.classList.add('active');
+    });
+    item.addEventListener('mouseout', function() {
+        const playBtn = item.querySelector('.section__item--play');
+        playBtn.classList.remove('active');
+    });
+}) 

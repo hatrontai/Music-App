9d416616eae6c572c6702385fb145caeeb04367e
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-11-03 14:16:36 +0700

    play music audio

--- assets/src/js/play.js
+++ assets/src/js/play.js
@@ -1,4 +1,5 @@
 const playBtns = $$('.fa-solid.fa-play')
+const audio = document.getElementById('audio')
 
 playBtns.forEach((btn) => {
     btn.addEventListener('click', function() {
@@ -6,5 +7,19 @@ playBtns.forEach((btn) => {
             btn.classList.toggle('fa-play');
             btn.classList.toggle('fa-pause');
         })
+        if(audio.paused) {
+            audio.play()
+        }
+        else {
+            audio.pause()
+        }
     })
 })
+
+audio.addEventListener('timeupdate', () => {
+    const progressBar = $('.progress')
+    const progressPercent = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.width = `${progressPercent}%`;
+    updateTimeDisplay();
+});
+

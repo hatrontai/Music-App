a0d929cb0ce4959ceb4c882a5329462b3229f942
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-11-03 23:49:36 +0700

    play audio music

--- assets/src/js/play.js
+++ assets/src/js/play.js
@@ -1,25 +1,46 @@
 const playBtns = $$('.fa-solid.fa-play')
 const audio = document.getElementById('audio')
 
+function updateTimeDisplay() {
+    const currentTime = audio.currentTime;
+    const duration = audio.duration;
+    const currentMinutes = Math.floor(currentTime / 60);
+    const currentSeconds = Math.floor(currentTime % 60);
+    const durationMinutes = Math.floor(duration / 60);
+    const durationSeconds = Math.floor(duration % 60);
+    $('.player__progress--current').textContent = `${currentMinutes}:${currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds}`;
+    $('.player__progress--duration').textContent = `${durationMinutes}:${durationSeconds < 10 ? `0${durationSeconds}` : durationSeconds}`;
+}
+
 playBtns.forEach((btn) => {
     btn.addEventListener('click', function() {
-        playBtns.forEach((btn) => {
-            btn.classList.toggle('fa-play');
-            btn.classList.toggle('fa-pause');
-        })
         if(audio.paused) {
             audio.play()
+            playBtns.forEach((btn) => {
+                btn.classList.remove('fa-play')
+                btn.classList.add('fa-pause')
+            })
         }
         else {
             audio.pause()
+            playBtns.forEach((btn) => {
+                btn.classList.remove('fa-pause')
+                btn.classList.add('fa-play')
+            })
         }
     })
 })
 
 audio.addEventListener('timeupdate', () => {
-    const progressBar = $('.progress')
+    const progressBar = $('.player__play--progress .progress')
     const progressPercent = (audio.currentTime / audio.duration) * 100;
     progressBar.style.width = `${progressPercent}%`;
     updateTimeDisplay();
+    if(audio.currentTime === audio.duration) {
+        playBtns.forEach((btn) => {
+            btn.classList.remove('fa-pause')
+            btn.classList.add('fa-play')
+        })
+    }
 });
 
